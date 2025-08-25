0848650e70edd5b43728abf9c436b3cd3b47d6e9
BaoBeili <105642445+BaoBeili@users.noreply.github.com>
2025-08-25 23:50:44 +0800

    Add files via upload

--- streamBiochar.py
+++ streamBiochar.py
@@ -2,7 +2,6 @@ import streamlit as st
 import numpy as np
 import pickle
 import joblib
-from  tensorflow.keras.models import load_model
 
 import   streamlit  as st; from PIL import Image; import numpy  as np
 import pandas  as pd; import pickle
